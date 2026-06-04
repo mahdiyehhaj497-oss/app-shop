74c54b3c3cafd3d92072ed3db8492dc1cb61682b
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-04 23:24:08 +0330

    build navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./compponents/shared/Navbar";
 
 
 export default function App() {
   return (
     <>
-  
+  <Navbar/>
   
     </>
   )
--- src/compponents/shared/Navbar.jsx
+++ src/compponents/shared/Navbar.jsx
@@ -1,7 +1,13 @@
 export default function Navbar() {
     return (
         <>
-        <div></div>
+            <div className="flex flex-row justify-center items-center mt-8 font-bold text-gray-500">
+                <span className="mx-4">Home</span>
+                <span className="mx-4">Singig</span>
+                <span className="mx-4">About Us</span>
+              <span className="mx-4">Contact Us</span>  
+
+        </div>
         </>
     )
 }
