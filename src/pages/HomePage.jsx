05209540a91f5e411fea181586c91ea139998cf4
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-05 00:25:02 +0330

    producpage and add some styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ export default function HomePage() {
         <div className="bg-[url('public/engin-akyurt-Hlkuojv_P6I-unsplash.jpg')] bg-no-repeat bg-cover bg-center h-screen">
           <Navbar />
 
-          <div >
+          <div className="flex justify-center items-center ">
             <Link to="/product">
-              <button>Show Producs</button>
+              <button className="mt-12 bg-white rounded-3xl h-10 w-40  font-bold text-gray-500 ">Show Producs</button>
             </Link>
           </div>
         </div>
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,8 +2,10 @@ import Product from "../compponents/Products";
 
 export default function ProductPage() {
     return (
-        <>
-        <Product/>
-        </>
-    )
+      <>
+        <div className="bg-[url('public/engin-akyurt-Hlkuojv_P6I-unsplash.jpg')] bg-no-repeat bg-cover bg-center h-screen">
+          <Product />
+        </div>
+      </>
+    );
 }
