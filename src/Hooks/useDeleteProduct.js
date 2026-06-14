7ad6000b1b0130c9f7de7194e5b610cb40355dc9
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-14 22:08:36 +0330

    build hook usedeleted product

---
+++ src/Hooks/useDeleteProduct.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
+
+export default function useDeleteProduct() {
+    const queryClient = useQueryClient()
+    
+    const mutationFn = async (productId) => {
+        const { data } = await api.delete(`products/${productId}`)
+        return data
+}
+
+    return (
+        useMutation({
+            mutationFn,
+            onSuccess: () => {
+                alert("delete product is success")
+                queryClient.invalidateQueries(["getallproduct"])
+            },
+            onError: () => {
+                alert("deleted is faild")
+            }
+        })
+    )
+} 
