21b96b98c6fd4e22e4ae7e7a3769d0138af2ae8b
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-15 17:52:47 +0330

    builde adminpanel page

--- src/compponents/AdminPanel.jsx
+++ src/compponents/AdminPanel.jsx
@@ -1,14 +1,80 @@
 import Footer from "./shared/Footer";
 import Navbar from "./shared/Navbar";
-
+import useGetProduct from "../Hooks/useGetProduct"
+import useDeleteProduct from "../Hooks/useDeleteProduct"
+import { useState } from "react";
+import usePostProduct from "../Hooks/usePostProduct";
+import { useForm } from "react-hook-form";
 export default function AdminPanel() {
+
+  const { data, isLoading } = useGetProduct()
+  const{mutateAsync:createMutation}=usePostProduct()
+const{mutate}=useDeleteProduct()
+  const [editing, setEditing] = useState(null)
+  const{register,reset, handleSubmit}=useForm()
+
+  function deleteFn(productId) {
+  mutate(productId)
+}
+
     return (
       <>
         <div className="bg-slate-800">
-          
-                <Navbar />
-                
-                
+          <Navbar />
+
+          <div className="flex flex-col items-center justify-center   ">
+            {data?.data.map((product) => (
+              <div
+                className="flex items-center justify-center rounded-3xl px-10 py-6 my-8 bg-slate-500 w-3/5"
+                key={product.id}
+              >
+                <img className="w-52" src={product.image} />
+                <div className="flex flex-col px-4">
+                  <span className="text-lg text-gray-900 font-semibold">
+                    {product.title}
+                  </span>
+                  <span className="font-semibold text-white">
+                    {product.description}
+                  </span>
+                  <div className="flex items-center justify-center mb-3 mt-3">
+                    <span className="font-bold text-2xl">{product.price}$</span>
+                  </div>
+
+                  <div className="flex justify-center items-center mx-6">
+                    <button
+                      onClick={() => deleteFn(product.id)}
+                      className="mx-6 h-8 w-20 bg-rose-950 text-white font-semibold rounded-2xl"
+                    >
+                      Deleted
+                    </button>
+                    <button
+                      onClick={() => {
+                        reset({
+                          title: product.title,
+                          description: product.description,
+                          price: product.price,
+                        });
+                        setEditing(product);
+                      }}
+                      className="mx-6 h-8 w-20 bg-yellow-500 text-white font-semibold rounded-2xl"
+                    >
+                      Edited
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center justify-center pb-8">
+            <button
+              className="mx-6 h-8 w-40 bg-green-800 text-white font-semibold rounded-2xl"
+              onClick={() => {
+                (reset(), setEditing(true));
+              }}
+            >
+              Add new Product
+            </button>
+          </div>
         </div>
 
         <Footer />
--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,10 +1,15 @@
 import { Mail, MapPin, Phone } from "lucide-react";
+import Navbar from "../compponents/shared/Navbar"
+import Footer from "../compponents/shared/Footer"
 
 export default function ContactUsPage() {
     return (
       <>
         <div className="bg-[url('public/quino-al-4SNUcHPiC8c-unsplash.jpg')] bg-center bg-cover h-screen flex items-center justify-center">
-          <div>
+          <div className="mb-32">
+            <div className="mb-40">
+              <Navbar />
+            </div>
             <div className="flex flex-row my-8">
               <div className="mx-4 text-gray-600  ">
                 <Phone size={48} />
@@ -15,7 +20,7 @@ export default function ContactUsPage() {
             </div>
             <div className="flex flex-row my-8">
               <div className="mx-4 text-gray-600 ">
-                <Mail size={48}/>
+                <Mail size={48} />
               </div>
               <div className="text-2xl font-semibold text-gray-500">
                 fake@example.com
@@ -23,14 +28,15 @@ export default function ContactUsPage() {
             </div>
             <div className="flex flex-row my-8">
               <div className="mx-4 text-gray-600 ">
-                <MapPin size={48}/>
+                <MapPin size={48} />
               </div>
               <div className="text-2xl font-semibold text-gray-500">
                 123 Street, Tehran,Iran
               </div>
             </div>
           </div>
-        </div>
+            </div>
+            <Footer/>
       </>
     );
 }
